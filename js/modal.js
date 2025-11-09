a48bbfadaf113fd66bed2bcf71453dd1e1a65e2a
AlexeyZ <azv99@yandex.ru>
2025-11-09 14:45:02 +0300

    2-ой урок + усложнённое из 2-ого

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,35 @@
 const modalBtn = document.querySelector('.modal__button')
 const modal = document.querySelector('.modal')
 
-
 modalBtn.addEventListener('click', () => {
   modal.style.display = 'flex'
+  const imgCross = document.createElement('img')
+  // Устанавливаем путь к изображению
+  imgCross.src = '../img/close.svg'
+  // Устанавливаем альтернативный текст для изображения
+  imgCross.alt = 'close'
+  // Устанавливаем стили для изображения
+  imgCross.classList.add('modal__close')
+  // Получаем форму, куда нужно вставить изображение
+  const modalInner = document.querySelector('.modal__inner')
+  // Вставляем изображение в форму
+  modalInner.append(imgCross)
+  // Получаем изображение, чтобы в последствии его удалить при закрытии модального окна
+  const closeBtn = modal.querySelector('.modal__close')
+  //Навешиваем слушателя на изображение
+  closeBtn.addEventListener('click', () => {
+    console.log(closeBtn)
+    modal.style.display = ''
+    closeBtn.remove()
+  })
 })
 
 modal.addEventListener('click', e => {
   const modelContent = e.target.closest('.modal__inner')
+  const closeBtn = modal.querySelector('.modal__close')
+
   if (!modelContent) {
     modal.style.display = ''
+    closeBtn.remove()
   }
 })
